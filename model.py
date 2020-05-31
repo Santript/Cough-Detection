64410677bc9f7938b5f2ff48cf75528a33907065
root <root@LAPTOP-F5HU9JVM.localdomain>
2020-05-31 10:13:14 -0700

    Fixing more issues

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 from numpy import loadtxt,genfromtxt
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Input, Conv1D, RNN, SimpleRNNCell, Activation, SimpleRNN, Lambda, Reshape
+from tensorflow.keras.layers import Dense, Input, Conv1D, RNN, SimpleRNNCell, Activation, SimpleRNN, Lambda, Reshape, LSTM, GRU
 from tensorflow.keras.optimizers import Adam
 import csv
 
@@ -11,13 +11,14 @@ with open("ESC-50/meta/esc50.csv") as f:
     reader = csv.reader(f)
     dataset = list(reader)
 
+#create spectrogram
 def make_spectrogram(signals):
     frame_length = tf.constant(2048)
     fft_length = tf.constant(512)
     spectrogram_complex = tf.signal.stft(signals, frame_length, fft_length)
     spectrogram = tf.math.abs(spectrogram_complex)
     #spectrogram = tf.transpose(spectrogram, perm=[0,2,1])
-    tf.math.abs
+    #tf.math.abs
     return spectrogram
 
 #creates model that will be trained with all the layers
@@ -36,11 +37,11 @@ def create_model():
     #forming the first convolutional layer for model
     model.add(Conv1D(conv_filters, conv_kernel_size))
     #forming the simple RNN layer for model where output is fed back to input
-    model.add(SimpleRNN(rnn_units))
+    model.add(GRU(rnn_units))
     #forming the Dense layer for model 
     model.add(Dense(output_size))
     #forming the activation or output layer for model
-    model.add(Activation('softmax'))
+    model.add(Activation('sigmoid'))
     
     #returning the final model with all the layers
     return model
@@ -48,15 +49,17 @@ def create_model():
 #training the model to improve accuracy
 def train_model(model,x_train,y_train):
     #learning rate(should decrease over time whilst training)
-    lr = 1e-4
+    #originally 1e-4
+    lr = 1e-5
     #displays how quickly the learning rate is decreasing
+    #originally 1e-6
     decay = 1e-6
     
     #sets up optimizer
     optim = Adam(lr=lr, decay=decay)
     #compiles the model showing loss and accuracy
     model.compile(optimizer=optim, loss = 'mse', metrics=['accuracy'])
-    model.fit(x_train,y_train,epochs = 10)
+    model.fit(x_train,y_train,epochs = 8)
 #%%
 
 import librosa as lib
@@ -66,28 +69,37 @@ import numpy as np
 #x,sr = lib.load(audio_data)
 #print(type(x), type(sr))
 model = create_model()
+#%%
+
 #creating empty list to store the audiofile array
 x_data = []
 y_data = []
 #this for loop stores each audio file in data set into an audio array and adds it to the list. Also, it checks if the category number matches 24, which is the category number for a cough.
-for row in dataset[1:20]:
-    print('I am a bad biyt')
+for row in dataset[1:]:
     audio_data = row[0]
     full_path = "./ESC-50/audio/" + audio_data
     x,sr = lib.load(full_path)
     x_data.append(x)
-    if row[3] == 24:
-        y_data.append(1)
-    else:
+    if row[2] == '24':
         y_data.append(0)
+    else:
+        y_data.append(1)
 x_data = np.asarray(x_data)
 y_data = np.asarray(y_data)
 
 #Using scikitlearn to split into training and testing data.
 from sklearn.model_selection import train_test_split
 
+#enables training to occur
 x_train,x_test,y_train,y_test = train_test_split(x_data,y_data,test_size=0.10)
-train_model(model,x_train,y_train)
+#%%
 
+print(y_data[:100])
+#%%
+train_model(model,x_train,y_train)
+#%%
 model.save("./tf-model")
+#%%
+#for row in dataset[1:]:
+    #print(row)
 
